aaaaef954cdc303cd1fcfca25b2f0b74e50a6c6d
Speed258 <erlandas252@gmail.com>
2020-03-23 22:00:08 +0200

    MQTT test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Formatter;
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Connect_With_New_Mqtt_Features().Wait();
+        }
+
+
+        public static async Task Connect_With_New_Mqtt_Features()
+        {
+            Console.WriteLine("GG");
+            var factory = new MqttFactory();
+            // This test can be also executed against "broker.hivemq.com" to validate package format.
+            var client = factory.CreateMqttClient();
+
+            var options = new MqttClientOptionsBuilder()
+                        .WithTcpServer("127.0.0.1", 1883)
+                        .WithProtocolVersion(MqttProtocolVersion.V500)
+                        .WithTopicAliasMaximum(20)
+                        .WithReceiveMaximum(20)
+                        .WithWillDelayInterval(20)
+                        .Build();
+            await client.ConnectAsync(options);
+
+            MqttApplicationMessage receivedMessage = null;
+
+            await client.SubscribeAsync("let/me/in/pep");
+
+           
+            Console.WriteLine("GG");
+            while (true)
+            {
+                await client.PublishAsync(new MqttApplicationMessage() { Topic = "let/me/in/pep", Payload = Encoding.ASCII.GetBytes("mysecretdata") });
+
+                client.UseApplicationMessageReceivedHandler(context => { receivedMessage = context.ApplicationMessage; Debug.WriteLine(receivedMessage.Payload.Length); });
+                await Task.Delay(500);
+            }
+        }
+    }
+}
---
+++ mqttweb/MQTTManager.cs
@@ -0,0 +1,109 @@
+﻿using MQTTnet;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using MQTTnet.Formatter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mqttweb
+{
+    public static class MQTTManager
+    {
+        public static IManagedMqttClient client;
+
+        public static async Task ConnectAsync()
+        {
+            string clientId = Guid.NewGuid().ToString();
+            string mqttURI = "127.0.0.1";
+            string mqttUser = "user12";
+            string mqttPassword = "userp2";
+            int mqttPort = 1883;
+            bool mqttSecure = false;
+            var messageBuilder = new MqttClientOptionsBuilder()
+                                    .WithClientId(clientId)
+                                    .WithCredentials(mqttUser, mqttPassword)
+                                    .WithTcpServer(mqttURI, mqttPort)
+                                    .WithProtocolVersion(MqttProtocolVersion.V500)
+                                    //.WithTls()
+                                    .WithCleanSession();
+
+            var options = mqttSecure ? messageBuilder.WithTls().Build() : messageBuilder.Build();
+            var managedOptions = new ManagedMqttClientOptionsBuilder()
+                                    .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+                                    .WithClientOptions(options)
+
+                                    .Build();
+
+            client = new MqttFactory().CreateManagedMqttClient();
+            await client.StartAsync(managedOptions);
+
+
+            
+            client.UseConnectedHandler(e =>
+            {
+                Console.WriteLine("Connected successfully with MQTT Brokers.");
+            });
+
+
+            await SubscribeAsync("+/+/+/+");
+            //await SubscribeAsync("let/me/in/pep");
+
+            Console.WriteLine(client.IsStarted + " " + client.IsConnected);
+
+            client.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("Disconnected from MQTT Brokers." + e.ClientWasConnected + " " + e?.AuthenticateResult?.ReasonString);
+            });
+
+            client.UseApplicationMessageReceivedHandler(e =>
+            {
+                try
+                {
+                    string topic = e.ApplicationMessage.Topic;
+                    if (string.IsNullOrWhiteSpace(topic) == false)
+                    {
+                        string payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                        Console.WriteLine($"Topic: {topic}. Message Received: {payload}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message, ex);
+                }
+            });
+
+        }
+
+        /// <summary>
+        /// Publish Message.
+        /// </summary>
+        /// <param name="topic">Topic.</param>
+        /// <param name="payload">Payload.</param>
+        /// <param name="retainFlag">Retain flag.</param>
+        /// <param name="qos">Quality of Service.</param>
+        /// <returns>Task.</returns>
+        public static async Task PublishAsync(string topic, string payload, bool retainFlag = true, int qos = 1) =>
+          await client.PublishAsync(new MqttApplicationMessageBuilder()
+            .WithTopic(topic)
+            .WithPayload(payload)
+            .WithQualityOfServiceLevel((MQTTnet.Protocol.MqttQualityOfServiceLevel)qos)
+            .WithRetainFlag(retainFlag)
+            .Build());
+
+
+        /// <summary>
+        /// Subscribe topic.
+        /// </summary>
+        /// <param name="topic">Topic.</param>
+        /// <param name="qos">Quality of Service.</param>
+        /// <returns>Task.</returns>
+        public static async Task SubscribeAsync(string topic, int qos = 1) =>
+          await client.SubscribeAsync(new TopicFilterBuilder()
+            .WithTopic(topic)
+            .WithQualityOfServiceLevel((MQTTnet.Protocol.MqttQualityOfServiceLevel)qos)
+            .Build());
+    }
+}
